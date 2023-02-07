754085fac6a021847e075ed02b3fc5afd6afc432
FlorinDorneanu <florindorneanuu1@gmail.com>
2023-02-07 19:35:13 +0000

    Declared variables for question data

---
+++ assets/js/script.js
@@ -0,0 +1,18 @@
+// - Questions game declared variables -
+
+const questionsGame = document.getElementById ("questions-container");
+const questionsAnswers = document.querySelectorAll (".answer");
+const questionTitle = document.getElementById ("question");
+const answer1_text = document.getElementById ("answer1_text");
+const answer2_text = document.getElementById ("answer2_text");
+const answer3_text = document.getElementById ("answer3_text");
+const answer4_text = document.getElementById ("answer4_text");
+const submitButton = document.getElementById ("btn");
+
+
+let currentQuestionsGame = 0;
+let score = 0;
+
+
+
+
