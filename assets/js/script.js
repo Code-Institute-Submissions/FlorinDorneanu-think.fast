a51dc346fac4eecf7bd58b3edbfba8b71eaa7972
FlorinDorneanu <florindorneanuu1@gmail.com>
2023-02-07 20:16:48 +0000

    Add function to select the answers

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,7 @@
+// - Import the questions -
+
+import {questionBox} from './questions.js'
+
 // - Questions game declared variables -
 
 const questionsGame = document.getElementById ("questions-container");
@@ -13,6 +17,39 @@ const submitButton = document.getElementById ("btn");
 let currentQuestionsGame = 0;
 let score = 0;
 
+// - Function to load the questions game -
+
+loadQuestionGame ();
+
+function loadQuestionGame () {
+
+    deselectAnswers();
+
+    const currentQuestionBox = questionBox[currentQuestionsGame];
+
+    questionTitle.innerText = currentQuestionBox.question;
+    answer1_text = currentQuestionBox.answer1;
+    answer2_text = currentQuestionBox.answer2;
+    answer3_text = currentQuestionBox.answer3;
+    answer4_text = currentQuestionBox.answer4;
+}
 
 
+// - Function to deselect the answers - 
+
+function deselectAnswers() {
+    questionsAnswers.forEach(questionsAnswers => questionsAnswers.checked = false);
+}
+
+// - Function to select the answers -
+
+function getSelected () {
+    let questionsAnswers;
+    questionsAnswers.forEach (questionsAnswers => {
+        if (questionsAnswers.checked) {
+            answer = questionsAnswers.id
+        }
+    })
+    return answer;
+}
 
