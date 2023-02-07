f9639ffe0bf6762a59691a9235a95d3f4f23f271
FlorinDorneanu <florindorneanuu1@gmail.com>
2023-02-07 22:09:16 +0000

    Add event listener to submit button

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,89 @@
+const questionBox = [
+    {
+        question : "How many bones are there in an adult human body?",
+        answer1 : "186",
+        answer2 : "206",
+        answer3 : "286",
+        answer4 : "306",
+        correct : "answer2"
+    },
+    {
+        question : "What is the longest river in the world?",
+        answer1 : "Amazon River",
+        answer2 : "Congo River",
+        answer3 : "Yellow River",
+        answer4 : "Nile",
+        correct : "answer4"
+    },
+    {
+        question : "In which country was the airline Ryanair founded?",
+        answer1 : "Germany",
+        answer2 : "Scotland",
+        answer3 : "Ireland",
+        answer4 : "Spain",
+        correct : "answer3"
+    },
+    {
+        question : "Who was assassinated on the 22nd of November 1963?",
+        answer1 : "Martin Luther King",
+        answer2 : "Jhon F Kennedy",
+        answer3 : "MK Gandhi",
+        answer4 : "Malcom X",
+        correct : "answer2"
+    },
+    {
+        question : "Which band released the song “Wonderwall” in the 90s?",
+        answer1 : "Oasis",
+        answer2 : "Joy Divison",
+        answer3 : "The Verge",
+        answer4 : "Nirvana",
+        correct : "answer1"
+    },
+    {
+        question : "What was Euclid?",
+        answer1 : "A philosopher",
+        answer2 : "A poet",
+        answer3 : "A painter",
+        answer4 : "A mathematician",
+        correct : "answer4"
+    },
+    {
+        question : "What is the capital of Iraq?",
+        answer1 : "Baghdad",
+        answer2 : "Islamabad",
+        answer3 : "Tehran",
+        answer4 : "Amman",
+        correct : "answer1"
+    },
+    {
+        question : "What colour is the “m” from the McDonald's logo?",
+        answer1 : "Blue",
+        answer2 : "Red",
+        answer3 : "Yellow",
+        answer4 : "Black",
+        correct : "answer3"
+    },
+    {
+        question : "What is guacamole made of?",
+        answer1 : "Banana",
+        answer2 : "Yoghurt",
+        answer3 : "Avocado",
+        answer4 : "Chick",
+        correct : "answer3"
+    },
+    {
+        question : "How many sides has a Hexagon?",
+        answer1 : "5",
+        answer2 : "6",
+        answer3 : "7",
+        answer4 : "8",
+        correct : "answer2"
+    },
+]
+
 // - Import the questions -
 
-import {questionBox} from './questions.js'
+//import {questionBox} from '../questions.js'
 
 // - Questions game declared variables -
 
@@ -19,19 +102,19 @@ let score = 0;
 
 // - Function to load the questions game -
 
-loadQuestionGame ();
+loadQuestionGame();
 
-function loadQuestionGame () {
+function loadQuestionGame() {
 
     deselectAnswers();
 
     const currentQuestionBox = questionBox[currentQuestionsGame];
 
     questionTitle.innerText = currentQuestionBox.question;
-    answer1_text = currentQuestionBox.answer1;
-    answer2_text = currentQuestionBox.answer2;
-    answer3_text = currentQuestionBox.answer3;
-    answer4_text = currentQuestionBox.answer4;
+    answer1_text.innerText = currentQuestionBox.answer1;
+    answer2_text.innerText = currentQuestionBox.answer2;
+    answer3_text.innerText = currentQuestionBox.answer3;
+    answer4_text.innerText = currentQuestionBox.answer4;
 }
 
 
@@ -45,11 +128,36 @@ function deselectAnswers() {
 
 function getSelected () {
     let questionsAnswers;
-    questionsAnswers.forEach (questionsAnswers => {
-        if (questionsAnswers.checked) {
+    questionsAnswers.forEach(questionsAnswers => {
+        if(questionsAnswers.checked) {
             answer = questionsAnswers.id
         }
     })
+
     return answer;
 }
+
+
+// - Create event listener to submit button -
+
+submitButton.addEventListener(click, () => {
+    const answer = getSelected()
+    if(answer) {
+        if(answer === questionBox[currentQuestionsGame].correct) {
+            score ++
+        }
+
+        currentQuestionsGame++
+
+        if(currentQuestionsGame < questionBox.lenght) {
+            loadQuestionGame()
+        } else {
+            questions-container.innerhtml = `
+            <h2>You answered ${score}/${questionBox.lenght} questions correctly </h2>
+
+            <button onclick="location.reload()">Reload</button>
+            `
+        }
+    }
+})
 
